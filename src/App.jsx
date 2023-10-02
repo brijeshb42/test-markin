b0df3f25079d79623a4c48b139879a947a008125
Brijesh Bittu <brijeshb42@gmail.com>
2023-10-02 06:19:43 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState, useRef } from 'react';
-import { Editor } from '@monaco-editor/react';
-import { Grid } from '@mui/material';
-import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import remarkGfm from 'remark-gfm';
-import examples from './examples';
-import SaveButton from './components/SaveButton';
-import DeployButton from './components/DeployButton';
-import FilenameInput from './components/FilenameInput';
+import { useState, useRef } from "react";
+import { Editor } from "@monaco-editor/react";
+import { Grid } from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+import remarkGfm from "remark-gfm";
+import examples from "./examples";
+import SaveButton from "./components/SaveButton";
+import DeployButton from "./components/DeployButton";
+import FilenameInput from "./components/FilenameInput";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   const editorRef = useRef(null);
   const modelRef = useRef(null);
   const [currentText, setCurrentText] = useState(examples["cheese"]);
-  const [filename, setFilename] = useState('Your Filename');
+  const [filename, setFilename] = useState("Your Filename");
 
   function handleSetFilename(newFilename) {
     setFilename(newFilename);
@@ -33,45 +33,49 @@ function App() {
     // setup monaco-vim
     window.require.config({
       paths: {
-        'vs': 'https://unpkg.com/monaco-editor/min/vs',
-        'monaco-vim': 'https://unpkg.com/monaco-vim/dist/monaco-vim',
-      }
+        vs: "https://unpkg.com/monaco-editor/min/vs",
+        "monaco-vim": "https://unpkg.com/monaco-vim/dist/monaco-vim",
+      },
     });
 
-    window.require(["monaco-vim"], function (MonacoVim) {
-      const statusNode = document.createElement('div');
-      statusNode.style.display = 'flex';
-      statusNode.style.position = 'fixed';
-      statusNode.style.zIndex = '3';
-      statusNode.style.bottom = '0';
-      statusNode.style.left = '0';
-      statusNode.style.color = 'white';
-      statusNode.style.background = '#2d2d30';
-      statusNode.style.width = '100%';
-      statusNode.style.fontSize = '13px';
-      statusNode.style.fontWeight = '500';
+    window.require(["monaco-vim"], function ({ initVimMode, VimMode }) {
+      const statusNode = document.createElement("div");
+      statusNode.style.display = "flex";
+      statusNode.style.position = "fixed";
+      statusNode.style.zIndex = "3";
+      statusNode.style.bottom = "0";
+      statusNode.style.left = "0";
+      statusNode.style.color = "white";
+      statusNode.style.background = "#2d2d30";
+      statusNode.style.width = "100%";
+      statusNode.style.fontSize = "13px";
+      statusNode.style.fontWeight = "500";
 
       // BLOCKED: This doesn't work, reached out to developer for help
       // statusNode.addEventListener('change', function() {
       //   VimMode.Vim.defineEx('write', 'w', clearText);
       // });
-
-      MonacoVim.initVimMode(editorRef.current, statusNode);
+      initVimMode(editorRef.current, statusNode);
+      VimMode.Vim.defineEx("write", "w", function () {
+        // your own implementation on what you want to do when :w is pressed
+        console.log(editor);
+        localStorage.setItem("editorvalue", editorRef.current.getValue());
+      });
 
       document.body.appendChild(statusNode);
     });
   }
 
   const options = {
-    "renderWhitespace": "all",
-    "autoIndent": true,
-    "fontSize": 16,
-    "wordWrap": "on",
-    "minimap": {
-      "enabled": true,
-      "autohide": true
+    renderWhitespace: "all",
+    autoIndent: true,
+    fontSize: 16,
+    wordWrap: "on",
+    minimap: {
+      enabled: true,
+      autohide: true,
     },
-  }
+  };
 
   return (
     <>
@@ -81,10 +85,13 @@ function App() {
           <SaveButton currentText={currentText} defaultFileName={filename} />
         </Grid>
         <Grid item>
-          <DeployButton currentText={currentText} defaultFilename={filename}/>
+          <DeployButton currentText={currentText} defaultFilename={filename} />
         </Grid>
         <Grid item>
-          <FilenameInput onInputSubmit={handleSetFilename} defaultFilename={filename} />
+          <FilenameInput
+            onInputSubmit={handleSetFilename}
+            defaultFilename={filename}
+          />
         </Grid>
       </Grid>
 
@@ -101,7 +108,7 @@ function App() {
               defaultLanguage="markdown"
               onMount={handleEditorDidMount}
               onChange={() => {
-                setCurrentText(editorRef.current.getValue())
+                setCurrentText(editorRef.current.getValue());
               }}
               options={options}
             />
@@ -110,17 +117,17 @@ function App() {
 
         {/* preview */}
         <Grid item md={6}>
-          <div style={{ height: '100vh', overflow: 'auto' }}>
+          <div style={{ height: "100vh", overflow: "auto" }}>
             <ReactMarkdown
               remarkPlugins={[remarkGfm]}
               children={currentText}
               components={{
                 code({ node, inline, className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || '')
+                  const match = /language-(\w+)/.exec(className || "");
                   return !inline && match ? (
                     <SyntaxHighlighter
                       {...props}
-                      children={String(children).replace(/\n$/, '')}
+                      children={String(children).replace(/\n$/, "")}
                       style={tomorrow}
                       language={match[1]}
                       PreTag="div"
@@ -129,15 +136,15 @@ function App() {
                     <code {...props} className={className}>
                       {children}
                     </code>
-                  )
-                }
+                  );
+                },
               }}
             />
           </div>
         </Grid>
       </Grid>
     </>
-  )
+  );
 }
 
 export default App;
